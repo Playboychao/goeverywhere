c15efde54241d186116f8b50beb69c973d209307
丁品超 <1007567595@qq.com>
2021-02-03 11:04:52 +0800

    0203

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'styles/reset.css'
 import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapper">
+    <swiper :options="swiperOptions">
+    <swiper-slide>
+        <img class="swiper-img" src="https://imgs.qunarzz.com/sight/p0/1712/19/19bc8bdb7062470a3.img.jpg_710x360_a9d2dcfb.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+        <img class="swiper-img" src="https://imgs.qunarzz.com/sight/p0/1712/72/727eab86aa7950c8a3.img.jpg_710x360_b70ce9a8.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+        <img class="swiper-img" src="https://imgs.qunarzz.com/sight/p0/1712/80/8046ad69c7c4b9fca3.img.jpg_710x360_514a456d.jpg">
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+    </div>
+</template>
+
+<script>
+    import 'swiper/css/swiper.css'
+    export default {
+        name:'HomeSwiper',
+        data () {
+            return {
+                swiperOptions: {
+                    pagination:{
+                        el:'.swiper-pagination'
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background:#fff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom:31.25%
+        background: #eee;
+        .swiper-img
+            width: 100%
+
+</style>
