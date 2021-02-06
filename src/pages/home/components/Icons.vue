f0254a54a5e0ce3b1c7a9a74adccd2e1efa258ef
丁品超 <1007567595@qq.com>
2021-02-06 08:32:18 +0800

    0206

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -60,37 +60,36 @@
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl';
 .icons
-      width: 100%;
-      overflow:hidden
-      height: 0
-      padding-bottom: 50% 
+    width: 100%;
+    overflow:hidden
+    height: 0
+    padding-bottom: 50% 
     .icon
-      position: relative;
-      overflow:hideen
-      float:left
-      width: 25%
-      height: 0;
-      padding-bottom:25%
-    .icon-img
-        position:absolute
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: .44rem;
-        box-sizing border-box
-        padding .1rem
-        background blue
-        .icon-imgcontent
-            display block
-            margin 0 auto
-            height: 100%;
-    .icon-desc
-        position:absolute
-        left: 0;
-        right: 0;
-        bottom: 0;
-        height: .44rem;
-        line-height .44rem
-        text-align center
-        color: $darkTextColor;
+        position: relative;
+        overflow:hideen
+        float:left
+        width: 25%
+        height: 0;
+        padding-bottom:25%
+        .icon-img
+            position:absolute
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: .44rem;
+            box-sizing border-box
+            padding .1rem
+            .icon-imgcontent
+                display block
+                margin 0 auto
+                height: 100%;
+        .icon-desc
+            position:absolute
+            left: 0;
+            right: 0;
+            bottom: 0;
+            height: .44rem;
+            line-height 1.35rem
+            text-align center
+            color: $darkTextColor;
 </style>
