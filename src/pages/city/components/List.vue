c6de1c6e9d46aafef21cab0108c9987ce4ecf115
丁品超 <1007567595@qq.com>
2021-02-09 22:24:49 +0800

    wrong

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list">
+        <div class="area">
+            <div class="title border-topbottom">当前城市</div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">热门城市</div>
+
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"CityList"
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.border-topbottom
+    &:before
+        border 1px solid #777
+    &:after
+        border 1px solid #777
+.title
+    line-height .44rem
+    background #eee
+    padding-left .2rem
+    color #666
+    font-size .26rem
+</style>
