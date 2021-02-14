9c0a463c0752a65b5992c60ecc48589698ed0e2e
丁品超 <1007567595@qq.com>
2021-02-14 21:03:54 +0800

    cannot ladong

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,12 +2,13 @@
     <div>
         <CityHeader></CityHeader>
         <CitySearch></CitySearch>
-        <CityList></CityList>
+        <CityList :cities="cities" :hot="hotCities"></CityList>
         <CityAlphabet></CityAlphabet>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -19,6 +20,29 @@ import CityAlphabet from './components/Alphabet'
             CitySearch,
             CityList,
             CityAlphabet
+        },
+        data(){
+            return{
+                cities:{},
+                hotCities:{}
+            }
+        },
+        methods:{
+            getCityInfo(){
+                axios.get('./mock/city.json')
+                .then(this.handleGetCityInfoSucc)
+            },
+            handleGetCityInfoSucc(res){
+                res = res.data
+                if(res.ret && res.data){
+                    const data = res.data
+                    this.cities = data.cities
+                    this.hotCities = data.hotCities
+                }
+            }
+        },
+        mounted(){
+            this.getCityInfo()
         }
     }
 </script>
