e91b3c619cb08f79a76692b937809481e57c3cb2
丁品超 <1007567595@qq.com>
2021-02-14 19:42:42 +0800

    can not right

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,6 +3,7 @@
         <CityHeader></CityHeader>
         <CitySearch></CitySearch>
         <CityList></CityList>
+        <CityAlphabet></CityAlphabet>
     </div>
 </template>
 
@@ -10,12 +11,14 @@
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
     export default {
         name:"City",
         components:{
             CityHeader,
             CitySearch,
-            CityList
+            CityList,
+            CityAlphabet
         }
     }
 </script>
