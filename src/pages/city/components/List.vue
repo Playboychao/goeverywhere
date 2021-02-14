fb87e779b14cddf2493db8fa159f21071ecc9b18
丁品超 <1007567595@qq.com>
2021-02-14 12:57:07 +0800

    0214

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -2,6 +2,11 @@
     <div class="list">
         <div class="area">
             <div class="title border-topbottom">当前城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+            </div>
         </div>
         <div class="area">
             <div class="title border-topbottom">热门城市</div>
@@ -21,15 +26,23 @@
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
-.border-topbottom
-    &:before
-        border-color #777
-    &:after
-        border-color #777
 .title
     line-height .44rem
     background #eee
     padding-left .2rem
     color #666
     font-size .26rem
+    border-top 1px solid #777
+.list:last-child
+    border-bottom 1px solid #777
+.button-list
+    padding .1rem
+    .button-wrapper
+        float left 
+        width: 33.33%;
+        .button
+            margin .1rem
+            text-align center
+            padding .1rem
+
 </style>
