edd8caa0948c908bec581f73a3943dcea9341ff6
丁品超 <1007567595@qq.com>
2021-02-15 16:58:01 +0800

    search finished

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -2,16 +2,29 @@
     <div>
         <div class="search">
             <input 
-            v-model="keyword" 
-            class="search-input" 
-            type="text" 
-            placeholder="输入城市名或拼音">
+                v-model="keyword" 
+                class="search-input" 
+                type="text" 
+                placeholder="输入城市名或拼音"
+            >
         </div>
-        <div class="search-content" ref="search"> 
+        <div 
+            class="search-content" 
+            ref="search"
+            v-show="keyword"
+        > 
             <ul>
-                <li class="search-item border-bottom" v-for="item of list"
+                <li class="search-item border-bottom"
+                 v-for="item of list"
+                :key='item.id'
                 >
                     {{item.name}}
+                </li>
+                <li 
+                class="search-item border-bottom" 
+                v-show="hasNoData"
+                >
+                    没有找到匹配数据
                 </li>
             </ul>
         </div>
@@ -32,16 +45,21 @@ import Bscroll from 'better-scroll'
                 timer:null
             }
         },
+        computed:{
+            hasNoData () {
+                return !this.list.length
+            }
+        },
         watch:{
-            keyword(){
+            keyword () {
                 if (this.timer) {
                     clearTimeout(this.timer)
                 }
                 this.timer = setTimeout(()=>{
                     const result = []
                     for (let i in this.cities){
-                        this.cities[i].foreach((value)=>{
-                            if (value.spallOf(this.keyword)>-1||value.name.indexOf(this.keyword)>-1){
+                        this.cities[i].forEach((value)=>{
+                            if (value.spell.indexOf(this.keyword)>-1||value.name.indexOf(this.keyword)>-1){
                                 result.push(value)
                             }
                         })
