77c125dee95bfde17027ee3376310da5a4d2ffc7
丁品超 <1007567595@qq.com>
2021-02-17 22:03:07 +0800

    detail-banner-finished

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <DetailBanner>
+        </DetailBanner>   
+    </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+    export default {
+        name:'Detail',  
+        components:{
+            DetailBanner
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
+import Detail from '@/pages/detail/Detail'
 Vue.use(VueRouter)
 
   const routes = [
@@ -14,8 +15,13 @@ Vue.use(VueRouter)
     path: '/city',
     name: 'City',
     component: City
- 
-},]
+  },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail,
+  },
+]
 
 const router = new VueRouter({
   routes
